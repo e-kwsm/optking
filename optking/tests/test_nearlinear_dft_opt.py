052a859ddbbaa96b00e8253fc7f0875d9d96699a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-10-31 10:46:24 +0900

    optking/tests/test_jsoninput.py optking/tests/test_linesearch.py optking/tests/test_nearlinear_dft_opt.py optking/tests/test_opthelper.py optking/tests/test_step_types.py optking/v3d.py versioneer.py

--- optking/tests/test_nearlinear_dft_opt.py
+++ optking/tests/test_nearlinear_dft_opt.py
@@ -2,7 +2,7 @@ import psi4
 import optking
 from .utils import utils
 
-#! B3LYP cc-pVDZ geometry optimzation of phenylacetylene, starting from
+#! B3LYP cc-pVDZ geometry optimization of phenylacetylene, starting from
 #! not quite linear structure
 def test_b3lyp_phenylacetylene(check_iter):
 
